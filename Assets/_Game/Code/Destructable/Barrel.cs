9573686b69f8ba9af5e95e3871e25f967952f772
alsunj <alsunj@taltech.ee>
2025-01-19 20:38:38 +0200

    Remove unnecessary calls

--- Assets/_Game/Code/Destructable/Barrel.cs
+++ Assets/_Game/Code/Destructable/Barrel.cs
@@ -14,27 +14,6 @@ public class Barrel : NetworkBehaviour, IDestrucable
         {
             _health.Value = _destructableSettings.health;
         }
-
-        _health.OnValueChanged += OnHealthChanged;
-    }
-
-    public override void OnDestroy()
-    {
-        _health.OnValueChanged -= OnHealthChanged;
-    }
-
-    private void OnHealthChanged(float oldHealth, float newHealth)
-    {
-        if (newHealth <= 0)
-        {
-            transform.DOScale(Vector3.zero, 0.5f).SetDelay(_destructableSettings.hitDelay)
-                .OnComplete(() => gameObject.SetActive(false));
-        }
-        else
-        {
-            transform.DOShakePosition(0.5f, _destructableSettings.shakeOffset, 10, 90, false, true)
-                .SetDelay(_destructableSettings.hitDelay);
-        }
     }
 
     [ClientRpc]
@@ -60,28 +39,13 @@ public class Barrel : NetworkBehaviour, IDestrucable
     private void RequestTakeDamageServerRpc(float damage)
     {
         _health.Value -= damage;
-        Debug.Log(_health.Value);
-        Debug.Log("health value at checking: " + _health.Value);
-
         if (_health.Value <= 0)
         {
-            CmdShowBarrelDestroyForEveryOneServerRpc();
+            RpcShowBarrelDestroyForEveryOneClientRpc();
         }
         else
         {
-            CmdShowBarrelDamageForEveryoneServerRpc();
+            RpcShowBarrelDamageForEveryoneClientRpc();
         }
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void CmdShowBarrelDestroyForEveryOneServerRpc()
-    {
-        RpcShowBarrelDestroyForEveryOneClientRpc();
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void CmdShowBarrelDamageForEveryoneServerRpc()
-    {
-        RpcShowBarrelDamageForEveryoneClientRpc();
     }
 }
